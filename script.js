223394adb1040305f14da7af5aa48b8b95e2642f
ojashwi shahi <ojashwisthakuri@gmail.com>
2023-03-30 07:31:33 +0545

    hh

---
+++ script.js
@@ -0,0 +1,17 @@
+console.log("javascript");
+// window.alert("hello");
+
+// // let answer=window.confirm("are you sure you want to exit");
+// let answer = window.prompt("what is the capital of nepal");
+// if(answer = "kathmandu");
+// {
+    
+//     window.alert("you are correct")
+
+
+// }
+// console.log(answer)
+window.document.body.style.backgroundColor="purple"     
+document.body.style.fontFamily="sans-serif"
+let movies = document.getElementsByClassName("movies")
+console.log(movies)
